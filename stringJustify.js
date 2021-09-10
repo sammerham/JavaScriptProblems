706cd4bf729a874ee8409c93dbec434ff8979929
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-09-10 09:47:15 -0700

    added extra comments

--- stringJustify.js
+++ stringJustify.js
@@ -7,16 +7,16 @@ function strJustify(line, length) {
   let spaces = words.length - 1;    // number of spaces in given str;
   // console.log('spaces', spaces)
   let neededSpaces = diff / spaces;   // number of needed spaces between words to make up the given length;
-  let newSpace = ' '.repeat(neededSpaces + 1);
-  let extraSpaces = diff % spaces;
-  let extraSpaceStr = ' '.repeat(extraSpaces);
+  let newSpace = ' '.repeat(neededSpaces + 1);  // convert num of spaces plus 1 for the normal single space between words into str of spaces
+  let extraSpaces = diff % spaces;  // if not given an even number and have a remianing space
+  let extraSpaceStr = ' '.repeat(extraSpaces);   // convert the num of extra spaces into str of spaces
   // console.log('extraSpaces', extraSpaces)
   // console.log('length new space', newSpace.length)
-  if (!extraSpaces) {
+  if (!extraSpaces) {    // if we don't have extra spaces, join the arr of words back into string on the new spaces and return 
     return words.join(newSpace)
-  } else {
-    words[0] += extraSpaceStr;
-    return words.join(newSpace);
+  } else {    // otherwise  add the extra space after the first word of the arr 
+    words[0] += extraSpaceStr;  
+    return words.join(newSpace);   // join the arr back on the new spaces and return
   }
 }
 
