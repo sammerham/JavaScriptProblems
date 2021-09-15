f171b131fffc05cf9abcf96f1d7c7bdca66145bd
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-09-15 16:06:38 -0700

    optimized bubble sort to avoid unnessary loops

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,15 +2,19 @@
 
 const arr = [2, 5, 6, 232, 66, 33, 55, 22, -1, 99, 9, 13, 15, 28, 30];
 
-const swap = (arr, idx1, idx2) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 
 const bubbleSort = arr => {
+  let noSwap;
   for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
     for (let j = 0; j < i - 1; j++) {
-      if(arr[j] > arr[j+1]) swap(arr, j, j + 1);
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwap = false;
+      }
     }
   }
   return arr;
 };
-  
 console.log(bubbleSort(arr));
