3452dd835f086aa644c80eda673a56f0febe32af
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-09-15 13:14:53 -0700

    refactored binarysearch

--- binarySearch.js
+++ binarySearch.js
@@ -1,20 +1,20 @@
 'use strict'
-const binarySearch = (arr, target) => {
+const binarySearch = (arr, num) => {
   let start = 0;
   let end = arr.length - 1;
   let mid = Math.floor((start + end) / 2);
 
-  while (arr[mid] !== target) {
-    if (arr[mid] < target) {
-      start = mid + 1;
-    } else {
+  // while (arr[mid] !== num && start <= end) {
+  while (start <= end) {
+    if (num < arr[mid]) {
       end = mid - 1;
+    } else {
+      start = mid + 1;
     }
     mid = Math.floor((start + end) / 2);
   }
-  if (arr[mid] === target) return mid;
+  if (arr[mid] === num) return mid;
   return -1;
-};
-
+}; 
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 5));
