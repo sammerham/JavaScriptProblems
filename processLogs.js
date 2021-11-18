225961e56f6e502ab32b31c55e045a4ebc265e2a
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-11-17 21:29:52 -0800

    changed variable name

--- processLogs.js
+++ processLogs.js
@@ -1,17 +1,17 @@
 'use strict'
 function processLogs(logs, maxSpan) {
-  const restut = [];
+  const result = [];
   const sortedLogs = logs.map(el => el.split(" ").map(el => +el ? +el : el)).sort((a, b) => a[0] - b[0]);
   for (let i = 0; i < sortedLogs.length - 1; i++) {
     let firstLog = sortedLogs[i];
     let secondLog = sortedLogs[i + 1];
     if (firstLog[0] === secondLog[0]) {
       if (Math.abs(firstLog[1] - secondLog[1]) <= maxSpan) {
-        restut.push(firstLog[0].toString());
+        result.push(firstLog[0].toString());
       }
     }
   }
-  return restut;
+  return result;
 }
 
 const logs = ["99 1 sign-in", "100 10 sign-in", "50 20 sign-in", "100 15 sign-out", "50 26 sign-out", "99 2 sign-out"]
